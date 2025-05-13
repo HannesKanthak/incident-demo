a9b24200b97730ec93909fcfa35f84164b425932
Hannes Kanthak <hannes.kanthak@implisense.com>
2025-05-13 22:41:14 +0200

    Refactor imports in IncidentAuditLogController and IncidentController for improved clarity

--- src/main/kotlin/demo/incident/controller/IncidentAuditLogController.kt
+++ src/main/kotlin/demo/incident/controller/IncidentAuditLogController.kt
@@ -3,7 +3,11 @@ package demo.incident.controller
 import demo.incident.dto.api.IncidentAuditLogResponse
 import demo.incident.service.IncidentAuditLogService
 import org.springframework.data.domain.Sort
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/incidents")
